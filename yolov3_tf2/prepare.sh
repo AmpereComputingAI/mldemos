5ac1f1a0e242f75dbd373b8c8e479c49fc802171
Frank Lee <frank@onspecta.com>
2021-09-14 23:28:35 +0200

    Install jupyterlab and opencv

--- yolov3_tf2/prepare.sh
+++ yolov3_tf2/prepare.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+pip install jupyterlab
+
+export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libGLdispatch.so.0.0.0
+
+pip install opencv-python
+
 # yolov3
 wget https://pjreddie.com/media/files/yolov3.weights -O data/yolov3.weights
 python convert.py --weights ./data/yolov3.weights --output ./checkpoints/yolov3.tf
