2fef64727689d8d66f9c2cd33cc448779727d43b
Frank Lee <frank@onspecta.com>
2021-09-20 23:23:29 +0200

    Modify number of threads

--- image_segmentation/start_jupyter_graviton.sh
+++ image_segmentation/start_jupyter_graviton.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DLS_NUM_THREADS=32 OMP_NUM_THREADS=32 TF_CPP_MIN_LOG_LEVEL=3  DLS_PROCESS_MODE=0 jupyter notebook --allow-root --port 8882
+DLS_NUM_THREADS=64 OMP_NUM_THREADS=64 TF_CPP_MIN_LOG_LEVEL=3  DLS_PROCESS_MODE=0 jupyter notebook --allow-root --port 8882
