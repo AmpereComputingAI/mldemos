a5b526834cd82f6ca799d828ce8bd923a7d2d2de
Frank Lee <frank@onspecta.com>
2021-09-21 23:26:55 +0200

    Modifications

--- ssd_mobilenet_v2/start_jupyter_altra.sh
+++ ssd_mobilenet_v2/start_jupyter_altra.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DLS_NUM_THREADS=32 OMP_NUM_THREADS=32 TF_CPP_MIN_LOG_LEVEL=3  DLS_PROCESS_MODE=1 numactl jupyter notebook --allow-root --port 8890
+DLS_NUM_THREADS=16 OMP_NUM_THREADS=16 TF_CPP_MIN_LOG_LEVEL=3  DLS_PROCESS_MODE=1 numactl jupyter notebook --allow-root --port 8890
