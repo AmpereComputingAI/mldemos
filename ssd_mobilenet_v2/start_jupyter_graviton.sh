c9b74ef711a2aa30be9c969e747799cf020fb9f2
Frank Lee <frank@onspecta.com>
2021-09-22 00:41:26 +0200

    Update graviton

--- ssd_mobilenet_v2/start_jupyter_graviton.sh
+++ ssd_mobilenet_v2/start_jupyter_graviton.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DLS_NUM_THREADS=32 OMP_NUM_THREADS=32 TF_CPP_MIN_LOG_LEVEL=3  DLS_PROCESS_MODE=0 jupyter notebook --allow-root --port 8880
+DLS_NUM_THREADS=48 OMP_NUM_THREADS=48 TF_CPP_MIN_LOG_LEVEL=3  DLS_PROCESS_MODE=0 numactl --physcpubind=0-47 jupyter notebook --allow-root --port 8880
