5967841521edd1ccffb0b1a9702f95b75aefee6f
Iyappan Subramanian <isubramanian@amperecomputing.com>
2022-08-10 10:40:12 -0700

    Change docker tag

--- yolov5-demo/start-docker.sh
+++ yolov5-demo/start-docker.sh
@@ -5,9 +5,8 @@ docker run \
   --privileged=true \
   -e "DISPLAY=$DISPLAY" \
   -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-  -v $HOME/yolo-demo:/workspace/yolo-demo:ro \
   --network host \
   --ipc host \
-  yolov5-pytorch-aio-demo:rev4
+  yolov5-pytorch-aio-demo:1.0
 
 # -v $HOME/yolo-demo:/workspace/yolo-demo \
