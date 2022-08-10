31f216b11d2e4d8ba13df26b4a94c6faa768fc4a
Iyappan Subramanian <isubramanian@amperecomputing.com>
2022-08-10 10:32:23 -0700

    Added command line arg check

--- yolov5-demo/build.sh
+++ yolov5-demo/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+if [ $# != 1 ]
+then
+	echo "Usage: $0 <tag>"
+	echo "recommended, tag = \`git tag | head -1\`"
+	exit
+fi
+
 name="yolov5-pytorch-aio-demo"
-tag="rev4"
+tag=$1
 
 docker build -t $name:$tag .
